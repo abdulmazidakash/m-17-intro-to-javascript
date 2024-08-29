360fc7c0a6a24ed0db91dc764847256d1d16c66e
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-08-29 21:42:14 +0600

    17-2 maths

---
+++ math.js
@@ -0,0 +1,19 @@
+/**
+ * ***************************************
+ * 		Math needed for programming 
+ * **************************************
+ * 
+ * 1.Number: +ve, -ve, 0, [50, -9, 0]
+ * 2.Integer: 2, 88, [odd, even]: 2, 4, 897
+ * 3.float: 5.6, 2.2
+ * 4.Operation: +, -, *, /
+ * 5.More Operation: %, [ ]
+ * 6.Percentage: 5%, 50 taka er 10% koto?
+ * 7.Sum: 14, 18, 16
+ * 8.Avg: 14, 18, 16
+ * 9.Max/Min: 14, 18, 16
+ * 10.Series:
+ * 11.Area: Triangle
+ * 12.
+ * 
+ */
