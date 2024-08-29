4d2af9b63cc05636455459deb60881237190e2c3
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-08-29 21:49:49 +0600

    17-3 what is a variable, five things you need to declare a variable

---
+++ variable.js
@@ -0,0 +1,14 @@
+/**
+ * variable ==> vary + able 
+ * 5 things to declare a variable 
+ */
+
+var weight = 38;
+var price = 38;
+var year = 1995;
+var money = 2000;
+
+var age;
+console.log(age);
+console.log(money);
+console.log(price);
