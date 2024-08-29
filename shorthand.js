613ef5715ada8ca9aa22ae670e0286696a6eb1d4
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-08-30 00:24:21 +0600

    17-9 mathmatical operation shorthand

---
+++ shorthand.js
@@ -0,0 +1,18 @@
+//addition
+var price = 35;
+price = price + 10;
+price += 10;
+// console.log(price);
+
+//subtraction
+var fridge = 35000;
+fridge = fridge - 1000;
+fridge -= 1000;
+// console.log(fridge);
+
+//multiply
+var orangePrice = 30;
+orangePrice = orangePrice * 3;
+orangePrice *= 3;
+console.log(orangePrice);
+
