300d5fb77e26878e8b73d0c59a1726ade425b131
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-08-30 00:18:59 +0600

    17-9 Mathmatical operation

---
+++ operation.js
@@ -0,0 +1,38 @@
+//Addition operation
+var onionPrice = 20;
+var eggPrice = 10;
+var total = onionPrice + eggPrice;
+
+// console.log(total);
+
+//subtraction operation
+var onionPrice = 20;
+var eggPrice = 10;
+var total = onionPrice - eggPrice;
+
+// console.log(total);
+
+//division operation
+var onionPrice = 20;
+var eggPrice = 10;
+var total = onionPrice / eggPrice;
+
+// console.log(total);
+
+//Multiplication operation
+var onionPrice = 20;
+var eggPrice = 10;
+var total = onionPrice * eggPrice;
+
+// console.log(total);
+
+
+//Modulus operation
+var onionPrice = 20;
+var eggPrice = 10;
+var total = onionPrice % eggPrice;
+
+console.log(total);
+
+
+
