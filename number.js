a0776ec28e223ba4bd642a046a923c1dc0aceade
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-08-30 00:12:53 +0600

    17-7 know the fundametals of js numbers

---
+++ number.js
@@ -0,0 +1,10 @@
+var orangePrice = 20;
+var chocolate = 0.5;
+var applePrice = '20';
+
+var applePrice = parseInt('Twenty');
+var applePrice = parseFloat('Twenty')
+
+console.log(typeof orangePrice);
+console.log(typeof applePrice);
+console.log(orangePrice + applePrice);
